25d9a1bf14b3452f7c6af88bd576f9bebc41d4d9
Baroshem <jakub.andrzejewski.dev@gmail.com>
2024-10-23 10:57:11 +0200

    feat: add title, meta, lang for SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt Starter',
+      meta: [
+        {
+          name: 'description', content: 'Opinionated Nuxt Starter'
+        }
+      ],
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
