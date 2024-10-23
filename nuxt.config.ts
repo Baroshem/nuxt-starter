42d6eed857b8cfc421b33d0367ddc7743ff4358e
Baroshem <jakub.andrzejewski.dev@gmail.com>
2024-10-23 10:58:09 +0200

    feat: add useful modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,4 +15,9 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    'nuxt-security',
+  ],
 })
